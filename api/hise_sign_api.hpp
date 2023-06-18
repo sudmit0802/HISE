54095abc63c4f70359ad17aea1c533e03a920cce
liwtw <irwinwillbeshaw@gmail.com>
2023-06-18 11:38:00 +0300

    Add basic implementation for test #1

--- api/hise_sign_api.hpp
+++ api/hise_sign_api.hpp
@@ -11,19 +11,19 @@ std::pair<Global_Escrow_HISE_PP, G2> setup(void)
 
     Global_Escrow_HISE_PP pp; // it's like a context for everything
 
-    G2 esk;  // super key for boss (he can use this key for decrypting any message)
+    G2 esk; // super key for boss (he can use this key for decrypting any message)
     Global_Escrow_HISE_Setup(pp, esk);
-    
+
     return std::make_pair(pp, esk);
 }
 
 std::pair<G1, G2> keygen(Global_Escrow_HISE_PP &pp)
 {
-    
+
     Fr sk; // random secret for generating dk
     G1 pk; // public key
-    Global_Escrow_HISE_KeyGen(pp, pk, sk); 
-    
+    Global_Escrow_HISE_KeyGen(pp, pk, sk);
+
     G2 dk; // private key
     Global_Escrow_HISE_Derive(pp, sk, dk);
 
@@ -34,7 +34,7 @@ Global_Escrow_HISE_CT encrypt(Global_Escrow_HISE_PP &pp, G1 &pk, std::string &me
 {
     // plain text
     GT pt;
-    
+
     std::string msg = message.substr(0, 32);
     std::string arg_str = "0b" + asciiToBinary(msg);
     arg_str += " 0 0 0 0 0 0 0 0 0 0 0";
@@ -46,50 +46,48 @@ Global_Escrow_HISE_CT encrypt(Global_Escrow_HISE_PP &pp, G1 &pk, std::string &me
     return ct;
 }
 
-
-    
-std::string decrypt(Global_Escrow_HISE_PP &pp, G2 &dk, Global_Escrow_HISE_CT &ct)   
+std::string decrypt(Global_Escrow_HISE_PP &pp, G2 &dk, Global_Escrow_HISE_CT &ct)
 {
     GT pt_prime; // decrypted text
-    Global_Escrow_HISE_Decrypt(pp, dk, ct, pt_prime); 
+    Global_Escrow_HISE_Decrypt(pp, dk, ct, pt_prime);
     std::string decrypted = pt_prime.getStr(2);
 
     std::vector<std::string> substrings = split(decrypted, ' ');
     std::string tmp_str = substrings.front();
     int number_of_lead_zeroes = 256 - tmp_str.length();
     std::string zeroes(number_of_lead_zeroes, '0');
-    tmp_str = zeroes+tmp_str;
+    tmp_str = zeroes + tmp_str;
     std::string ret_str = binaryToAscii(tmp_str);
     return ret_str;
 }
 
-
-std::string super_decrypt(Global_Escrow_HISE_PP &pp, G2 &esk, Global_Escrow_HISE_CT &ct)   
+std::string super_decrypt(Global_Escrow_HISE_PP &pp, G2 &esk, Global_Escrow_HISE_CT &ct)
 {
-    
+
     GT pt_prime; // decrypted text
     Global_Escrow_HISE_Escrow_Decrypt(pp, esk, ct, pt_prime);
     std::string decrypted = pt_prime.getStr(2);
+
     std::vector<std::string> substrings = split(decrypted, ' ');
     std::string tmp_str = substrings.front();
     int number_of_lead_zeroes = 256 - tmp_str.length();
-    std::string zeroes('0', number_of_lead_zeroes);
-    tmp_str = zeroes+tmp_str;
+    std::string zeroes(number_of_lead_zeroes, '0');
+    tmp_str = zeroes + tmp_str;
     std::string ret_str = binaryToAscii(tmp_str);
     return ret_str;
 }
 
 G2 sign(Fr &sk, std::string &msg)
 {
-    G2 sig; 
+    G2 sig;
     Global_Escrow_HISE_Sign(sk, msg, sig);
 
     return sig;
 }
 
 bool verify(Global_Escrow_HISE_PP &pp, G1 &pk, std::string &msg, G2 &sig)
-{ 
-    return Global_Escrow_HISE_Verify(pp, pk, msg, sig);  
+{
+    return Global_Escrow_HISE_Verify(pp, pk, msg, sig);
 }
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,105 @@
-//#define LOG
+// #define LOG
 
 #include "../api/hise_sign_api.hpp"
 
-int main()
+// Basic test. Boss can decrypt common communication member's messages
+void test_1()
 {
+    std::cout << "Start test #1" << std::endl;
+
     auto setup_pair = setup();
     auto ctx = std::get<0>(setup_pair);
     auto super_key = std::get<1>(setup_pair);
+
+    std::cout << std::endl;
 
     auto keys = keygen(ctx);
     auto public_key = std::get<0>(keys);
     auto private_key = std::get<1>(keys);
 
-    std::string msg("hi");
+    std::cout << std::endl;
+    std::cout << std::endl;
+    std::cout << std::endl;
+    std::cout << std::endl;
+    std::cout << std::endl;
 
-    auto cipher_text = encrypt(ctx, public_key, msg);
-    auto decrypted_text = decrypt(ctx, private_key, cipher_text);
-    
-    std::cout<<decrypted_text<<std::endl;
+    std::string msg_ref("TEST STRING FOR ENCRYPTION");
+
+    std::string msg_1(msg_ref);
+    auto cipher_text_1 = encrypt(ctx, public_key, msg_1);
+    std::cout << "Encrypted by COMMON: " << cipher_text_1.Y2 << std::endl;
+    auto decrypted_text_1 = decrypt(ctx, private_key, cipher_text_1);
+    std::cout << "Decrypted by COMMON: " << decrypted_text_1 << std::endl;
+
+    std::cout << std::endl;
+    std::cout << std::endl;
+    std::cout << std::endl;
+    std::cout << std::endl;
+    std::cout << std::endl;
+
+    std::string msg_2(msg_ref);
+    auto cipher_text_2 = encrypt(ctx, public_key, msg_2);
+    std::cout << "Encrypted by SUPER: " << cipher_text_2.Y2 << std::endl;
+    auto decrypted_text_2 = super_decrypt(ctx, super_key, cipher_text_2);
+    std::cout << "Decrypted by SUPER: " << decrypted_text_2 << std::endl;
+
+    std::cout << "Finish test #1" << std::endl
+              << std::endl;
+    return;
+}
+
+// Boss can not forge a signature
+void test_2()
+{
+    std::cout << "Start test #2" << std::endl;
+
+    std::cout << "Finish test #2" << std::endl
+              << std::endl;
+
+    return;
+}
+
+// Boss can communicate with others as typical user using his super key (signing and encrypting)
+void test_3()
+{
+
+    std::cout << "Start test #3" << std::endl;
+
+    std::cout << "Finish test #3" << std::endl
+              << std::endl;
+
+    return;
+}
+
+// User addition to system, expand key set keeping boss key valid
+void test_4()
+{
+    std::cout << "Start test #4" << std::endl;
+
+    std::cout << "Finish test #4" << std::endl
+              << std::endl;
+
+    return;
+}
+
+// Check key compromise
+void test_5()
+{
+    std::cout << "Start test #5" << std::endl;
+
+    std::cout << "Finish test #5" << std::endl
+              << std::endl;
+
+    return;
+}
+
+int main(int argc, char **argv)
+{
+    test_1();
+    test_2();
+    test_3();
+    test_4();
+    test_5();
 
     return 0;
 }
