e981d74873ad3392c7e7e73fbc355d415481861a
liwtw <irwinwillbeshaw@gmail.com>
2023-06-18 12:32:57 +0300

    Remove unused files

--- build/CMakeFiles/main.dir/DependInfo.cmake
+++ build/CMakeFiles/main.dir/DependInfo.cmake
@@ -1,14 +1,16 @@
-
-# Consider dependencies only in project.
-set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
-
 # The set of languages for which implicit dependencies are needed:
 set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
   )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/mnt/c/Users/User/Desktop/HISE/src/main.cpp" "/mnt/c/Users/User/Desktop/HISE/build/CMakeFiles/main.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
 
-# The set of dependency files which are needed:
-set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "/mnt/c/Users/sudmi/Desktop/kmzi/HISE/src/main.cpp" "CMakeFiles/main.dir/src/main.cpp.o" "gcc" "CMakeFiles/main.dir/src/main.cpp.o.d"
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include"
   )
 
 # Targets to which this target links.
